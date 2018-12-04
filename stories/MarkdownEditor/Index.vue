b8166afa92800bec807b19ff8837bc3085854836
echoLC <651644100@qq.com>
2018-12-04 15:31:55 +0800

    Add the stories markdown editor

---
+++ stories/MarkdownEditor/Index.vue
@@ -0,0 +1,110 @@
+<template>
+  <div :id="id"></div>
+</template>
+
+<script>
+import 'codemirror/lib/codemirror.css' // codemirror
+import 'tui-editor/dist/tui-editor.css' // editor ui
+import 'tui-editor/dist/tui-editor-contents.css' // editor content
+import Editor from 'tui-editor'
+import defaultOptions from './defaultOptions'
+
+export default {
+  name: 'MarkdownEditor',
+  props: {
+    value: {
+      type: String,
+      default: ''
+    },
+    id: {
+      type: String,
+      required: false,
+      default () {
+        return 'markdown-editor-' + (+new Date()) + ((Math.random() * 1000).toFixed(0) + '')
+      }
+    },
+    options: {
+      type: Object,
+      default () {
+        return defaultOptions
+      }
+    },
+    mode: {
+      type: String,
+      default: 'markdown'
+    },
+    height: {
+      type: String,
+      required: false,
+      default: '300px'
+    },
+    language: {
+      type: String,
+      required: false,
+      default: 'zh_CN' // https://github.com/nhnent/tui.editor/tree/master/src/js/langs
+    }
+  },
+  data () {
+    return {
+      editor: null
+    }
+  },
+  computed: {
+    editorOptions () {
+      const options = Object.assign({}, defaultOptions, this.options)
+      options.initialEditType = this.mode
+      options.height = this.height
+      options.language = this.language
+      return options
+    }
+  },
+  watch: {
+    value (newValue, preValue) {
+      if (this.isEditorValueChange(newValue, preValue)) {
+        this.setValue(newValue)
+      }
+    },
+    height (newValue) {
+      this.editor.height(newValue)
+    },
+    mode (newValue) {
+      this.editor.changeMode(newValue)
+    }
+  },
+  mounted () {
+    this.initEditor()
+  },
+  destroyed () {
+    this.destroyEditor()
+  },
+  methods: {
+    initEditor () {
+      this.editor = new Editor({
+        el: document.getElementById(this.id),
+        ...this.editorOptions
+      })
+      if (this.value) {
+        this.setValue(this.value)
+      }
+      this.editor.on('change', () => {
+        this.$emit('input', this.getValue())
+      })
+    },
+    destroyEditor () {
+      if (!this.editor) return
+      this.editor.off('change')
+      this.editor.remove()
+    },
+    setValue (value) {
+      this.editor.setValue(value)
+    },
+    getValue () {
+      return this.editor.getValue()
+    },
+    isEditorValueChange (newVal, preVal) {
+      return (newVal !== preVal) && (newVal !== this.getValue())
+    }
+  }
+}
+</script>
+
---
+++ stories/MarkdownEditor/markdown.stories.js
@@ -0,0 +1,34 @@
+import { storiesOf } from '@storybook/vue';
+// import { action } from '@storybook/addon-actions';
+
+import MarkdownEditor from './Index.vue';
+
+const defaultValue = '## 标题';
+const defaultOptions = {
+  toolbarItems: ['heading', 'bold', 'italic']
+};
+
+storiesOf('MarkdownEditor', module)
+  .add('default editor', () => ({
+    components: { MarkdownEditor },
+    template: '<markdown-editor></markdown-editor>'
+  }))
+  .add('inital value editor', () => ({
+    components: { MarkdownEditor },
+    template: `<markdown-editor :value="defaultValue"></markdown-editor>`,
+    data: () => ({ defaultValue })
+  }))
+  .add('En language editor', () => ({
+    components: { MarkdownEditor },
+    template: `<markdown-editor language="en_US"></markdown-editor>`
+  }))
+  .add('simple editor', () => ({
+    components: { MarkdownEditor },
+    template: `<markdown-editor :options="defaultOptions"></markdown-editor>`,
+    data: () => ({ defaultOptions })
+  }))
+  .add('500px height editor', () => ({
+    components: { MarkdownEditor },
+    template: `<markdown-editor height="500px"></markdown-editor>`,
+    data: () => ({ defaultOptions })
+  }));
