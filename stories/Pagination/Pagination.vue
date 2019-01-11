fd1a98cc5411681bab5f59ca800d70c2b0401d13
echoLC <651644100@qq.com>
2019-01-11 17:07:55 +0800

    Add the pagination component

---
+++ stories/Pagination/Pagination.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>分页组件</div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
---
+++ stories/Pagination/pagination.stories.js
@@ -0,0 +1,8 @@
+import { storiesOf } from '@storybook/vue'
+
+import Pagination from './Pagination.vue'
+
+storiesOf('Pagination', module).add('defaultPagination', () => ({
+  components: { Pagination },
+  template: `<pagination/>`
+}))
