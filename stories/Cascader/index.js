a8ed52dc98fd68bb832cf38407fd88c8d1b5dd6c
echoLC <651644100@qq.com>
2019-06-12 14:44:25 +0800

    feat: 添加了级联组件

---
+++ stories/Cascader/index.js
@@ -0,0 +1,1 @@
+export { default } from './src/Cascader.vue'
---
+++ stories/Cascader/src/Cascader.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="dv-cascader">
+    <h1>级联组件</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DvCascader',
+  
+  props: {
+    options: {
+      type: Array,
+      default: () => ([])
+    }
+  },
+
+  data () {
+    return {}
+  },
+
+  methods: {},
+
+  created () {
+    console.log(this.options)
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
